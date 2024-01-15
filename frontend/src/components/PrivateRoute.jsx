05a030fa1e70a479cc15d23291b99b7e9ddb17fb
Preeti biswas <preeti.biswas@brovitech.com>
2024-01-16 01:54:33 +0530

    addPrivateRouteForLogindUser

---
+++ frontend/src/components/PrivateRoute.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useAuthStatus } from '../hooks/useAuthStatus'
+import { Navigate,Outlet } from 'react-router-dom'
+import Spinner from './Spinner'
+const PrivateRoute = () => {
+    const { loggedIn, checkingStatus } = useAuthStatus()
+
+    if(checkingStatus) {return <Spinner/>}
+  return (
+    loggedIn ? <Outlet/> : <Navigate to='/login'/>
+  )
+}
+
+export default PrivateRoute
