32bc2b4419f81f82ba6f8036c8cb4e9b72cd4a03
Preeti biswas <preeti.biswas@brovitech.com>
2024-01-15 23:46:43 +0530

    get&createTickets

--- backend/controller/ticketController.js
+++ backend/controller/ticketController.js
@@ -5,13 +5,41 @@ const Ticket = require('../models/ticketModel')
 // get user Ticket,api/tickets,private routes
 
 const getTicket = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'getTickets' })
+  // get user useing the id in the jwt
+  const user = await User.findById(req.user.id)
+  if (!user) {
+    res.status(401)
+    throw new Error('user not Found')
+  }
+  const tickets = await Ticket.find({ user: req.user.id })
+
+  res.status(200).json(tickets)
 })
 
 // create newTicket private, post:/api/tickets
 
 const createTicket = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'createTicket' })
+  const { product, description } = req.body
+
+  if (!product || !description) {
+    res.status(400)
+    throw new Error('Please add a Product and description')
+  }
+  // get user using id in the jwt
+  const user = await User.findById(req.user.id)
+  if (!user) {
+    res.status(401)
+    throw new Error('User not Found')
+  }
+
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status: 'new',
+  })
+
+  res.status(200).json(ticket)
 })
 
 module.exports = { getTicket, createTicket }
