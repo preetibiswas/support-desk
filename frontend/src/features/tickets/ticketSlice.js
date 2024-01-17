d4a9ab3d66e8e3322ce154be3f13f9644679d0c5
Preeti biswas <preeti.biswas@brovitech.com>
2024-01-17 17:03:25 +0530

    singleTicketDisplayDOne

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -45,6 +45,25 @@ export const getTickets = createAsyncThunk(
     }
   },
 )
+// get singleTicket
+
+export const getTicket = createAsyncThunk(
+  'ticket/get',
+  async (ticketId, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token
+      return await ticketService.getTicket(ticketId, token)
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+      return thunkAPI.rejectWithValue(message)
+    }
+  },
+)
 
 const ticketSlice = createSlice({
   name: 'tickets',
@@ -75,6 +94,19 @@ const ticketSlice = createSlice({
         state.tickets = action.payload
       })
       .addCase(getTickets.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+      })
+      .addCase(getTicket.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(getTicket.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.ticket = action.payload
+      })
+      .addCase(getTicket.rejected, (state, action) => {
         state.isLoading = false
         state.isError = true
         state.message = action.payload
